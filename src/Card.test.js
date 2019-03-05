ad3c2c05e73db8e72c507b3130d72a77d7b379e4
Ryan Jeske <ryanjeske@gmail.com>
2019-03-05 15:45:48 -0700

    added Jest smoke and snapshot tests

---
+++ src/Card.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Card from './Card';
+
+describe('Card component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card title="First Card" content="lorem ipsum"/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Card title="First Card" content="lorem ipsum"/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+});
+
---
+++ src/List.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import List from './List';
+
+describe('List component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List 
+        key={1}
+        header="First List"
+        cards={[{ title: 'First card', content: 'lorem ipsum' }, { title: 'Second card', content: 'lorem ipsum' }, { title: 'Eighth card', content: 'lorem ipsum' }]}
+    />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List 
+        key={1}
+        header="First List"
+        cards={[{ title: 'First card', content: 'lorem ipsum' }, { title: 'Second card', content: 'lorem ipsum' }, { title: 'Eighth card', content: 'lorem ipsum' }]}
+    />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+});
