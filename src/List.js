b79482e9f740b21bc418466680e67c4d210d36c8
Ryan Jeske <ryanjeske@gmail.com>
2019-03-05 09:13:47 -0700

    initial commit

---
+++ src/List.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card'
+import './List.css';
+
+export default function List(props) {
+  console.log(props.cards);
+    return (
+    <section className='List'>
+      <header className='List-header'>
+        <h2>{props.header}</h2>
+      </header>
+      <div className='List-cards'>
+        {props.cards.map((card) =>
+          <Card
+            key={card.id}
+            title={card.title}
+            content={card.content}
+          />
+        )}
+      </div>
+    </section>
+  )
+}
