c5bc3cf37ab05331d80b9b98dcce73eced41fe91
Ryan Jeske <ryanjeske@gmail.com>
2019-03-09 16:27:55 -0700

    added delete and random card buttons

--- src/List.js
+++ src/List.js
@@ -3,8 +3,7 @@ import Card from './Card'
 import './List.css';
 
 export default function List(props) {
-  console.log(props.cards);
-    return (
+  return (
     <section className='List'>
       <header className='List-header'>
         <h2>{props.header}</h2>
@@ -13,11 +12,24 @@ export default function List(props) {
         {props.cards.map((card) =>
           <Card
             key={card.id}
+            id={card.id}
             title={card.title}
             content={card.content}
+            onClickDelete={props.onClickDelete}
           />
         )}
+        <button
+          type='button'
+          className='List-add-button'
+          onClick={() => props.onClickAdd(props.id)}
+        >
+          + Add Random Card
+        </button>
       </div>
     </section>
   )
+}
+
+List.defaultProps = {
+  onClickAdd: () => {},
 }
